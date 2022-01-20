e4be85ed6fff9f4f362e33b37986876b61adbfb2
Manishrai05 <68022978+Manishrai05@users.noreply.github.com>
2022-01-20 22:57:25 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,8 @@
+from home.models import Contact,Product,Orders
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Product)
+admin.site.register(Orders)
